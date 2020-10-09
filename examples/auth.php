d8fadb11474224e0d11dafb91a775010b4ce2ec2
abel16 <wenxuefei@daiba.com>
2020-10-09 11:32:32 +0800

    修改例子

--- examples/auth.php
+++ examples/auth.php
@@ -3,6 +3,8 @@
 require '../vendor/autoload.php';
 
 use Alipay\EasySDK\Kernel\Factory as MiniProgram;
+use Alipay\EasySDK\Kernel\Config;
+use Alipay\EasySDK\Kernel\Util\ResponseChecker;
 
 $config = require 'config.php';
 if (empty($config)) {
@@ -14,9 +16,17 @@ if (empty($config)) {
  * 
  * @var MiniProgram $miniProgram
  */
-MiniProgram::setOptions($config);
+$conf = new Config($config);
+MiniProgram::setOptions($conf);
 $auth = MiniProgram::base()->oauth();
 $ret = $auth->getToken('xxx');
+
+$responseChecker = new ResponseChecker();
+if ($responseChecker->success($ret)) {
+    echo 'ok';
+} else {
+    echo $ret->msg . $ret->subMsg;
+}
 
 // code2session
 //$ret = $miniProgram->auth->session('');
